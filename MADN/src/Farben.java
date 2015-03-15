d3f67e653f0dd5d5b0ce5346b47796531adabed7
Katherina <Katherina@kathi>
2015-03-15 21:14:17 +0100

    Klasse Spieler überarbeitet

--- MADN/src/Farben.java
+++ MADN/src/Farben.java
@@ -1,4 +1,6 @@
 
 public enum Farben {
+	
+	ROT, BLAU, GELB, GRUEN;
 
 }
--- MADN/src/Spieler.java
+++ MADN/src/Spieler.java
@@ -1,11 +1,71 @@
 
 public class Spieler {
 
-	private Wuerfel wu;
-	
-	private Spielfigur sf;
 	
 	private String name;
+	private Wuerfel wuerfel;
+	private static final int spielfigurMax =4 ;
+	private static Spielfigur[] spielfigur = new Spielfigur[spielfigurMax];
+	private static int spielfigurAnz=0;
+	private Farben farbeFigur;
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+
+	  	if ((name==null)||(name.length()<2)){
+	  	throw new IllegalArgumentException("setName:ungültige Eingabe");
+		
+	  	}
+	  	this.name=name;
+	}  	
+	
+	
+	public Farben getFarbeFigur() {
+		return farbeFigur;
+	}
+
+	public void setFarbeFigur(Farben farbeFigur) {
+		this.farbeFigur=farbeFigur;
+
+
+	}  
+	
+	public Spieler(String name, Farben farbeFigur){
+		setName(name);
+		setFarbeFigur(farbeFigur);
+		
+	}
+	
+	
+	public static void figurHinzufuegen(Spielfigur sf){
+		
+		if(spielfigurAnz < spielfigurMax){
+			 spielfigur [spielfigurAnz++]=sf;
+		}
+	}
+	
+
+	public static int getAnzahlFiguren() {
+		return spielfigurAnz;
+	}
+	
+	@Override
+	public String toString(){
+		return "SpielerName "+getName()+", "+getName()+" mit der Farbe "+getFarbeFigur();
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if (!(o instanceof Spieler)) return false;
+		Spieler s=(Spieler)o;
+		return (s.getName()==this.getName());
+	}
+
 	
 	
 	
