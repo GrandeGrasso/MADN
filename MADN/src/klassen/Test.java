557e6f759e475198396603e33a274532306fdb9a
StefanoGrasso <Stefano.Grasso@student.reutlingen-university.de>
2015-03-17 16:37:34 +0100

    Konflikt

--- MADN/src/klassen/Test.java
+++ MADN/src/klassen/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Ich bin der Yunus");
     System.out.println("Ich bin der Stefano");
+    System.out.println("asdasd");
 	}
 
 }
