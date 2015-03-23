cfd2105aad52e3246a6fa1ecc9c1d0e2cf377f35
StefanoGrasso <Stefano.Grasso@student.reutlingen-university.de>
2015-03-24 00:06:03 +0100

    Javadoc Bugfixes 2

--- MADN/src/klassen/eFarben.java
+++ MADN/src/klassen/eFarben.java
@@ -2,23 +2,14 @@ package klassen;
 import java.util.Random;
 
 
+/**
+ * Farben Enum, Definiert die Spielfarben
+ * @author Gruppe B-5
+ * @version 1.0
+ *
+ */
+
 public enum eFarben {
-	
-	/**
-	* Farben Enum, Definiert die Spielfarben
-	* @author Gruppe B-5
-	* @version 1.0
-	*
-	*/
-	
-	/**
-	 * Attribute
-	 * @param ROT Farbe Rot
-	 * @param BLAU Farbe Blau
-	 * @param GELB Farbe Gelb
-	 * @param GRUEN Farbe Grün
-	 * 
-	 */
 	
 	ROT, BLAU, GELB, GRUEN;
 
--- MADN/src/klassen/eFeldTyp.java
+++ MADN/src/klassen/eFeldTyp.java
@@ -1,5 +1,12 @@
 package klassen;
 
+/**
+ * Feldtyp Enum, Definiert die Spielfeldtypen
+ * @author Gruppe B-5
+ * @version 1.0
+ *
+ */
+
 public enum eFeldTyp {
 
 	Startfeld, Zielfeld, Lauffeld, Homefeld;
