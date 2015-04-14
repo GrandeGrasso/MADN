2c516e6630118fffa555a92f1eb8353407f67c3a
Katherina <Katherina@kathi>
2015-04-14 12:12:59 +0200

    ki

---
+++ MADN/src/klassen/KI_Aggressiv.java
@@ -0,0 +1,10 @@
+package klassen;
+
+public class KI_Aggressiv extends KI {
+
+	public KI_Aggressiv(Spieler s) {
+		super(s);
+		
+	}
+
+}
---
+++ MADN/src/klassen/KI_Defensiv.java
@@ -0,0 +1,11 @@
+package klassen;
+
+public class KI_Defensiv extends KI{
+
+	public KI_Defensiv(Spieler s) {
+		super(s);
+	
+	}
+	
+
+}
